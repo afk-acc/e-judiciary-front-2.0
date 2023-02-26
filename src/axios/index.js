e443a6f05db9ed4f4187d30171c81c80df9ae899
okta <ai.devgeneration>
2023-02-26 18:32:32 +0500

    asd

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-        baseURL: "https://api.e-judiciaru.uz/api/v1/",
+        baseURL: "https://api.e-judiciary.uz/api/v1/",
         headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`
         },
