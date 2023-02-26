f8e7ab1def70db0bf49c102ca6c0e9128e52cc44
okta <ai.devgeneration>
2023-02-26 19:54:39 +0500

    asd

--- src/store/index.js
+++ src/store/index.js
@@ -15,7 +15,7 @@ export default createStore({
     },
     getters:{
         get_base_url(state) {
-            return 'https://api.e-judiciary.uz/'
+            return 'http://api.e-judiciary.uz/'
         }
     },
     mutations:{},
