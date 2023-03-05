52f1d8b4114339999d378d48caf3918362c231ba
okta <ai.devgeneration>
2023-03-05 14:20:45 +0500

    asdas

--- src/pages/profile/profileInfo.vue
+++ src/pages/profile/profileInfo.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="w-full max-lg:mt-16">
-    <h1 class="text-link font-bold text-2xl">{{ $t("Основная информация") }}</h1>
+    <div class="flex gap-x-4 my-2">
+      <h1 class="text-link font-bold text-2xl">{{ $t("Основная информация") }}</h1>
+      <button v-if="getCurrentUser.role_name === 'user' " @click="send_request_lawyer(getCurrentUser.id)" class="text-white bg-primary_gr py-2 px-4 rounded-xl ">{{$t("Стать юристом")}}</button>
+    </div>
+
     <div v-if="item = getCurrentUser.id" class="w-full mx-auto max-md:flex max-md:flex-col">
       <profile-info-item :item="getCurrentUser.name" :label="$t('ФИО')" />
       <profile-info-item :item="$t('Ташкент')" :label="$t('Узбекистан')" />
@@ -14,7 +18,7 @@
 
 <script>
 import ProfileInfoItem from "../../components/profile/profileInfoItem.vue";
-import { mapGetters } from "vuex";
+import {mapActions, mapGetters} from "vuex";
 import profileInfoScelet from "./sceleton/profileInfoScelet.vue";
 
 export default {
@@ -23,6 +27,9 @@ export default {
     ...mapGetters(["getCurrentUser"]),
   },
   mounted() {},
+  methods:{
+    ...mapActions(['send_request_lawyer'])
+  },
 };
 </script>
 
