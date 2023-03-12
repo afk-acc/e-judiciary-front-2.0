c685b445c73f0006e60c4534e00c0518e9e0a268
okta <ai.devgeneration>
2023-03-12 12:42:12 +0500

    asd

--- src/components/profile/document_template/documentPreview.vue
+++ src/components/profile/document_template/documentPreview.vue
@@ -2,13 +2,14 @@
   <div class="max-w-[700px] max-lg:w-[720p] my_class"
   >
     <span v-for="(content, index) in item.doc_content"
-          class=" inline-block "
+          class=""
           :class="{
       'text-justify':content.align === 'start',
       'text-center':content.align === 'center',
       'text-end':content.align === 'end',
       'font-bold':content.weight === 'bold',
       'block w-full ':Number(content.new_line) === 1,
+
       'italic':content.italic
 
     }"
@@ -17,9 +18,9 @@
           v-if="!(content.deleted) "
       >
           <br v-if="Number(content.new_line) === 1"/>
-        <div class="break-all" style="white-space: pre-line">
-          {{content.text}}
-         </div>
+        <span class="break-all" style="white-space: pre-line">
+          {{ content.text }}
+         </span>
           <span class="break-all" v-for="field in content.fields">
                {{ field.value }}
           </span>
