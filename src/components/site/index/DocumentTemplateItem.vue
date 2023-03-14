6ca3c6bd0cbb2a029317d03a7e6c851614885aaa
okta <ai.devgeneration>
2023-03-14 19:01:32 +0500

    sad

--- src/components/site/index/DocumentTemplateItem.vue
+++ src/components/site/index/DocumentTemplateItem.vue
@@ -19,7 +19,7 @@
           <svg width="5" height="5" viewBox="0 0 5 5" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="2.5" cy="2.5" r="2.5" fill="white"/>
           </svg>
-          <span class="break-all" :class="{' text-left':isEnd}">
+          <span class="break-words" :class="{' text-left':isEnd}">
 
             {{ child.name}}
           </span>
